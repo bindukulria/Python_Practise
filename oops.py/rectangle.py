d5cf46e994271262edb2fda3198d6a72133cff3c
bindu <jjaat7002@gmail.com>
2026-08-12 11:36:27 +0530

    updated rectangle code

---
+++ oops.py/rectangle.py
@@ -0,0 +1,39 @@
+from quadrila import Quadrilateral 
+
+class Rectangle(Quadrilateral):#parent class is Quadrilateral
+
+    count=0
+
+    def __init__(self,length,breadth):
+        Rectangle.count += 1
+        super().__init__(length,breadth,length,breadth)
+
+        self.id = Rectangle.count
+        self.length = length
+        self.breadth = breadth
+
+        def area(self):
+            print("I am area of Rectangle")
+            return self.length * self.breadth
+        
+        def __add__(self,r):
+            print("I am method of Rectangle")
+            return self.area() >= r.area()
+
+        def __str__(self):
+            return f"Rectangle ID: {self.id} Dimension is {self.length}X{self.breadth}"
+
+        r1 = Rectangle(10,5)
+
+        print(r1.area())
+        print(r1)
+
+
+
+
+
+
+
+
+
+
---
+++ oops.py/usermypack1.py
@@ -0,0 +1,5 @@
+from rectangle import Rectangle
+r = Rectangle(10,5)
+ 
+print("Perimeter:",r.perimeter())
+print("Area:",r.area())
