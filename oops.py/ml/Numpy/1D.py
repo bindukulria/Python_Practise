f997ec1023c771922f0f8046cd15efc7bb113208
bindu <jjaat7002@gmail.com>
2026-09-01 12:30:19 +0530

    Add numpy statistics program

---
+++ oops.py/ml/Numpy/1D.py
@@ -0,0 +1,32 @@
+# import numpy as np
+
+#marks = np.array([75,80,65,90,55])
+# 
+# print(marks)
+# print(type(marks))
+# print(marks.ndim)
+# print(marks.shape)
+# 
+
+# 
+
+# import numpy as np
+
+# marks = np.array([75,80,65,90,55])
+
+# print(marks + 5)
+# print(marks - 10)
+# print(marks * 2)
+# print(marks / 2)
+
+
+import numpy as np
+
+marks = np.array([75,80,65,90,55])
+
+print("Mean:",np.mean(marks))
+print("Median:",np.median(marks))
+print("Max:",np.max(marks))
+print("Min:",np.min(marks))
+print("Std Dev:",np.std(marks))
+
