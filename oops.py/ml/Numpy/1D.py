a05352918d789fa6a467108e543c9651b7632aa8
bindu <jjaat7002@gmail.com>
2026-09-02 12:56:59 +0530

    Update pandas ML program

--- oops.py/ml/Numpy/1D.py
+++ oops.py/ml/Numpy/1D.py
@@ -1,23 +1,20 @@
-# import numpy as np
+import numpy as np
 
-#marks = np.array([75,80,65,90,55])
-# 
-# print(marks)
-# print(type(marks))
-# print(marks.ndim)
-# print(marks.shape)
-# 
+marks = np.array([75,80,65,90,55])
+ 
+print(marks)
+print(type(marks))
+print(marks.ndim)
+print(marks.shape)
 
-# 
 
-# import numpy as np
 
-# marks = np.array([75,80,65,90,55])
+marks = np.array([75,80,65,90,55])
 
-# print(marks + 5)
-# print(marks - 10)
-# print(marks * 2)
-# print(marks / 2)
+print(marks + 5)
+print(marks - 10)
+print(marks * 2)
+print(marks / 2)
 
 
 import numpy as np
@@ -29,4 +26,5 @@ print("Median:",np.median(marks))
 print("Max:",np.max(marks))
 print("Min:",np.min(marks))
 print("Std Dev:",np.std(marks))
+
 
---
+++ oops.py/ml/Numpy/2d.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+student_data = np.array([
+    [85,4,70],
+    [92,5,82],
+    [60,2,45],
+    [75,3,60]
+    ])
+
+print(student_data)
+print(student_data.shape)
+
+
+import numpy as np
+
+marks = np.array([75,80,65,90,55])
+
+print(marks[0])
+print(marks[1])
+print(marks[-1])
+
+
+import numpy as np
+
+student_data = np.array([
+    [80,4,70],
+    [92,5,82],
+    [60,2,45],
+])
+
+print(student_data[0,0])
+print(student_data[1,2])
+
+
